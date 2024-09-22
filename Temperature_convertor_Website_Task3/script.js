c56377e5c8eb646a4d12a7fa3b7e16dee20acd96
CODEparas <parasjaiin1234@gmail.com>
2024-09-22 13:11:26 -0400

    learning

---
+++ Temperature_convertor_Website_Task3/script.js
@@ -0,0 +1,20 @@
+function convertTemperature() {
+    const inputTemperature = parseFloat(document.getElementById("num").value);
+    const unit = document.getElementById("unit").value;
+
+    let convertedTemperature;
+
+    if (unit == 'Celsius') {
+        //convert to Fahrenheit
+        convertedTemperature = (inputTemperature * 9 / 5) + 32;
+        document.getElementById("result").textContent = `${inputTemperature} degrees Celsius=${convertedTemperature} degrees Fahrenheit.`
+    }
+    else if (unit === 'Fahrenheit') {
+        //Convert to celsius
+        convertedTemperature = (inputTemperature - 32) * 5 / 9;
+        document.getElementById("result").textContent = `${inputTemperature} degrees Fahrenheit=${convertedTemperature} degrees Celsius.`
+    }
+    else {
+        document.getElementById("result").textContent = 'Please Select a valid unit.';
+    }
+}
